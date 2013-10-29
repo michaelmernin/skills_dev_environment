267a8da83ec32d99805582137bf8d6979d5df967
Palmer <palmer.liao@H2000-0150L.perficient.com>
2013-10-29 19:22:32 +0800

    get current code

--- sites/all/modules/mail_sender/templates/selectPeers.tpl.php
+++ sites/all/modules/mail_sender/templates/selectPeers.tpl.php
@@ -2,7 +2,7 @@
   Dear [username],<br>
   <br>
   [reviewname] has begun.The review will end on [datetime].<br>
-  You can start to fill up your peers-review form by clicking this link of the Enterprise Review System 
+  You can fill up your peer-review form for [employeeName] by clicking this link of the Enterprise Review System 
   <a href='[peersurl]'>[peersurl]</a><br>
   <br>
   Thank you, <br>
