3775bbf91bf3bd0a7f85e17b1efa36f3a4aee37d
Mars Shen <mars.shen@perficient.com>
2013-11-06 15:00:53 +0800

    add new module for custom webform field and now we can create new node when we start a new review

--- sites/all/modules/mail_sender/templates/new_review_started_counselee.tpl.php
+++ sites/all/modules/mail_sender/templates/new_review_started_counselee.tpl.php
@@ -2,8 +2,9 @@
   Dear [username],<br>
   <br>
   [reviewname] has begun.The review will end on [datetime].<br>
-  You can start to fill up your self-review form and select peers to collect feedback by clicking this link of the Enterprise Review System 
+  You can start to fill up your self-review form <a href='[reviewurl]'>[reviewurl]</a> and select peers to collect feedback by clicking this link of the Enterprise Review System 
   <a href='[peersurl]'>[peersurl]</a><br>
+  
   <br>
   Thank you, <br>
   Enterprise Review System 
