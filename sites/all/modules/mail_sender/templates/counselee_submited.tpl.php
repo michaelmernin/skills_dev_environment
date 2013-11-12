4bc244021885d67b1cbde12eaf7c97c99711c1db
Mars Shen <mars.shen@perficient.com>
2013-11-12 14:20:54 +0800

    fix some bug and change email template

--- sites/all/modules/mail_sender/templates/counselee_submited.tpl.php
+++ sites/all/modules/mail_sender/templates/counselee_submited.tpl.php
@@ -1,5 +1,8 @@
 <div>
   Dear [username],<br>
   This is automatically generated email to notify you that the [reviewname] is currently submitted by the [counselee]. <br>
-  Click Link:<a herf='[url]'>[url]</a> to watch [counselee]'s currently result.
+  Click Link: <a href='[url]'>[url]</a> to watch [counselee]'s currently result.<br>
+  <br>
+  Thank you, <br>
+  Enterprise Review System 
 </div>
