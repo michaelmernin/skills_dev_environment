4ef9e85e9551c5ca9ed6dbb5878a74ebb812ed1c
unknown <palmer.liao@H2000-0150L.perficient.com>
2013-11-20 12:26:37 +0800

    delete files

--- sites/all/modules/mail_sender/templates/counselee_submited.tpl.php
+++
@@ -1,8 +0,0 @@
-<div>
-  Dear [username],<br>
-  This is automatically generated email to notify you that the [reviewname] is currently submitted by the [counselee]. <br>
-  Click Link: <a href='[url]'>[url]</a> to watch [counselee]'s currently result.<br>
-  <br>
-  Thank you, <br>
-  Enterprise Review System 
-</div>
