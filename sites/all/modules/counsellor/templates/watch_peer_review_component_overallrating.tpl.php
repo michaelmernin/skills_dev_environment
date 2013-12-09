e780db81a0dce4fb369043ba422c99a99b655eb7
Mars Shen <mars.shen@perficient.com>
2013-12-09 15:48:53 +0800

    fix some small bugs 1. pie chart N/A display 2.change overall rating to select type in peer review

---
+++ sites/all/modules/counsellor/templates/watch_peer_review_component_overallrating.tpl.php
@@ -0,0 +1,5 @@
+<div class="webform-submission-info clearfix">
+  <div style="font-weight: 600;float: left;padding-right: 5px;padding-left: 5px;"><?php print $component_content->name ?></div>
+  <div class="color-rating-box"><?php print $component_content->answer ?></div>
+</div>
+<hr/>
--- sites/all/modules/counsellor/templates/watch_peer_review_component_select.tpl.php
+++ sites/all/modules/counsellor/templates/watch_peer_review_component_select.tpl.php
@@ -1,12 +1,10 @@
 <div class="webform-submission-info clearfix">
-  <div><?php print $component_content->name ?></div>
+  <div style="margin-top: 3px;margin-bottom: 3px"><?php print $component_content->name ?></div>
   <div class="webform-submission-info-text">
-    <div style="font-weight: 600;float: left">·Comment | </div>
-    <div style="font-weight: 600;float: left;padding-right: 5px;padding-left: 5px;">·Rating:</div><div class="color-rating-box"><?php print $component_content->answer ?></div></div>
-
+    <div style="font-weight: 600;float: left;margin-bottom: 2px">·Comment | </div>
+    <div style="font-weight: 600;float: left;padding-right: 5px;padding-left: 5px;margin-bottom: 2px">·Rating:</div><div class="color-rating-box"><?php print $component_content->answer ?></div></div>
   <div class="additionalbubble">
     <div style="padding: 0 10px 5px;margin-bottom: 5px;">
-
       <?php print $component_content->comment ?>
       <input type="hidden" id="hidPeerRviewCid"value=" <?php print $component_content->cid ?>"/>
     </div>
