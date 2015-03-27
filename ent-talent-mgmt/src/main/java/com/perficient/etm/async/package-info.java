73c8b973c714555b97a81f1d0fb6ce7fc5e6dbd7
Donovan Mikrot <donovan.mikrot@perficient.com>
2015-03-27 12:01:24 -0500

    jhipster scaffold with mysql and without compass

---
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/async/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Async helpers.
+ */
+package com.perficient.etm.async;
---
+++ ent-talent-mgmt/src/main/webapp/scripts/app/account/logout/logout.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('etmApp')
+    .controller('LogoutController', function (Auth) {
+        Auth.logout();
+    });
