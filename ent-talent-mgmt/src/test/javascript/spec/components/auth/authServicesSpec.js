4fb523903de4c35840acb20f7cc3c6291b5b0d94
Donovan Mikrot <donovan.mikrot@perficient.com>
2015-08-06 18:07:55 -0500

    Add new script expectation to karma test

--- ent-talent-mgmt/src/test/javascript/spec/components/auth/authServicesSpec.js
+++ ent-talent-mgmt/src/test/javascript/spec/components/auth/authServicesSpec.js
@@ -18,6 +18,7 @@ describe('Services Tests ', function () {
       $httpBackend.expectGET('i18n/en/global.json').respond(200, '');
       $httpBackend.expectGET('i18n/en/language.json').respond(200, '');
       $httpBackend.expectGET('scripts/components/toolbar/toolbar.html').respond({});
+      $httpBackend.expectGET('scripts/components/sidenav/sidenav.html').respond({});
       $httpBackend.expectGET('i18n/en/global.json').respond(200, '');
       $httpBackend.expectGET('i18n/en/language.json').respond(200, '');
       $httpBackend.expectGET('i18n/en/main.json').respond(200, '');
