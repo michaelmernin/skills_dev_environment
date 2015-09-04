d41d86c47e144ffc66fe2141bd64bafa10852f94
michaelkaplan <michael.kaplan@perficient.com>
2015-09-04 10:35:29 -0500

    Finished development on the annual shell page.

--- ent-talent-mgmt/src/main/webapp/scripts/app/entities/review/review-edit.controller.js
+++ ent-talent-mgmt/src/main/webapp/scripts/app/entities/review/review-edit.controller.js
@@ -1,12 +1,16 @@
 'use strict';
 
-angular.module('etmApp')
-    .controller('ReviewEditController', function ($scope, $stateParams, Review, ReviewType, ReviewStatus, User) {
-        $scope.review = {};
-        $scope.load = function (id) {
-            Review.get({id: id}, function(result) {
-              $scope.review = result;
-            });
-        };
-        $scope.load($stateParams.id);
+angular.module('etmApp').controller('ReviewEditController', function ($scope, $stateParams, Review, ReviewType, ReviewStatus, User) {
+  $scope.review = {};
+  $scope.load = function (id) {
+    Review.get({id: id}, function(result) {
+      $scope.review = result;
     });
+  };
+  
+  if ($stateParams.review) {
+    $scope.review = $stateParams.review;
+  } else {
+    $scope.load($stateParams.id);
+  }
+});
