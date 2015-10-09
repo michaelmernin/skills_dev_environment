4c5e3d013c5a56b07c4bcaa331095723287ef7cf
michaelkaplan <michael.kaplan@perficient.com>
2015-10-09 11:01:12 -0500

    Finished up goals

---
+++ ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/goals/goal.detail.controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('etmApp').controller('GoalDetailController', function ($scope, $mdDialog, goal) {
+  $scope.goal = goal;
+  $scope.switchCompletion = goal.completionDate ? true : false;
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  
+  $scope.hide = function() {
+    if ($scope.goalForm.$valid) {
+      $mdDialog.hide($scope.goal);
+    }
+  };
+  
+  $scope.isPopulated = function() {
+    if ($scope.goal.completionDate) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+});
--- ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/goals/goals.controller.js
+++ ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/goals/goals.controller.js
@@ -1,12 +1,56 @@
 'use strict';
 
-angular.module('etmApp').controller('GoalsController', function ($scope, $stateParams, Goal) {
+angular.module('etmApp').controller('GoalsController', function ($scope, $mdDialog, $stateParams, Goal) {
   var review = {};
   $scope.goals = [];
 
   $scope.$parent.$watch('review', function (parentReview) {
     review = parentReview;
-    $scope.goals = review.goals;
+    $scope.goals = review.goals || [];
   });
+  
+  $scope.addGoal = function (ev) {
+    $mdDialog.show({
+      controller: 'GoalDetailController',
+      templateUrl: 'scripts/components/entities/review/goals/goal.detail.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      locals: {
+        goal: new Goal()
+      }
+    })
+    .then(function (goal) {
+      goal.$save();
+      $scope.goals.push(goal);
+    });
+  };
+  
+  $scope.editGoal = function (goal, ev) {
+    $mdDialog.show({
+      controller: 'GoalDetailController',
+      templateUrl: 'scripts/components/entities/review/goals/goal.detail.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      locals: {
+        goal: goal
+      }
+    })
+    .then(function (goal) {
+      goal.$update();
+    });
+  };
+  
+  $scope.deleteGoal = function (goal, ev) {
+    var confirmDelete = $mdDialog.confirm()
+      .title('Confirm Goal Deletion')
+      .ariaLabel('Delete Goal Confirm')
+      .content('Delete Goal: ' + goal.name + '?')
+      .ok('Delete')
+      .cancel('Cancel')
+      .targetEvent(ev);
+    $mdDialog.show(confirmDelete).then(function () {
+      $scope.goals.splice($scope.goals.indexOf(goal), 1);
+    });
+  };
   
 });
