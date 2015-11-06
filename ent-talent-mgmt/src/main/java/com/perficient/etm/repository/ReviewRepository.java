f7c5e1b1356cc36db9e553a452fc1751fe2928b4
Ahmed Musallam <ahmed.musallam@perficient.com>
2015-11-06 11:15:34 -0600

    postfilter and postauthorize on find* in review repository

--- ent-talent-mgmt/src/main/java/com/perficient/etm/repository/ReviewRepository.java
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/repository/ReviewRepository.java
@@ -6,10 +6,11 @@ import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 
 import com.perficient.etm.domain.Review;
-import com.perficient.etm.security.PostFilterUrtils;
+import com.perficient.etm.security.PrePostUtils;
 
 /**
  * Spring Data JPA repository for the Review entity.
@@ -20,9 +21,13 @@ public interface ReviewRepository extends JpaRepository<Review,Long>{
      * Will return a list of completed project reviews for a reviewee
      * with a review endDate that is between startDate and endDate.
      */
+    @PostFilter(PrePostUtils.POST_FILTER_REVIEWS)
     @Query("select r from Review r where r.reviewType.interval = 'PROJECT' and r.reviewStatus.id = 5 and r.reviewee.id = :userId and r.endDate > :startDate and r.endDate <= :endDate")
     List<Review> findCompletedEngagementsForUserWithinDates(@Param("userId") Long userId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
-    @PostFilter(PostFilterUrtils.RESTRICT_REVIEWS)
+    @PostFilter(PrePostUtils.POST_FILTER_REVIEWS)
     List<Review> findAll();
+    
+    @PostAuthorize(PrePostUtils.POST_AUTHORIZE_REVIEW)
+    Review findOne(Long id);
 }
--- ent-talent-mgmt/src/main/java/com/perficient/etm/security/PrePostUtils.java
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/security/PrePostUtils.java
@@ -1,9 +1,14 @@
 package com.perficient.etm.security;
 
-public class PostFilterUrtils {
+public class PrePostUtils {
 
-	public static final String RESTRICT_REVIEWS = "filterObject.reviewee.login == principal.username"
+	public static final String POST_FILTER_REVIEWS = "filterObject.reviewee.login == principal.username"
 												+ " or filterObject.reviewer.login == principal.username"
 												+ " or filterObject.reviewee.counselor.login == principal.username"
 												+ " or filterObject.reviewee.generalManager.login == principal.username";
+	
+	public static final String POST_AUTHORIZE_REVIEW = "returnObject != null ? returnObject.reviewee.login == principal.username"
+			+ " or returnObject.reviewer.login == principal.username"
+			+ " or returnObject.reviewee.counselor.login == principal.username"
+			+ " or returnObject.reviewee.generalManager.login == principal.username : true";
 }
