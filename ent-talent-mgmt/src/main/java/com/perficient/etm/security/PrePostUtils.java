70b5523bfaf2d874bed0f65bda11aef79825180b
Ahmed Musallam <ahmed.musallam@perficient.com>
2015-11-08 21:15:54 -0600

    including peer security into postAuthentication of reviews

--- ent-talent-mgmt/src/main/java/com/perficient/etm/security/PrePostUtils.java
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/security/PrePostUtils.java
@@ -5,11 +5,13 @@ public class PrePostUtils {
 	public static final String POST_FILTER_REVIEWS = "filterObject.reviewee.login == principal.username"
 												+ " or filterObject.reviewer.login == principal.username"
 												+ " or filterObject.reviewee.counselor.login == principal.username"
-												+ " or filterObject.reviewee.generalManager.login == principal.username";
+												+ " or filterObject.reviewee.generalManager.login == principal.username"
+												+ " or (filterObject.peers.![login].contains(principal.username) and filterObject.reviewStatus.id < 4) ";
 	
 	public static final String POST_AUTHORIZE_REVIEW = "returnObject == null"
 													+ " or returnObject.reviewee.login == principal.username"
 													+ " or returnObject.reviewer.login == principal.username"
 													+ " or returnObject.reviewee.counselor.login == principal.username"
-													+ " or returnObject.reviewee.generalManager.login == principal.username";
+													+ " or returnObject.reviewee.generalManager.login == principal.username"
+													+ " or (returnObject.peers.![login].contains(principal.username) and returnObject.reviewStatus.id < 4) ";
 }
