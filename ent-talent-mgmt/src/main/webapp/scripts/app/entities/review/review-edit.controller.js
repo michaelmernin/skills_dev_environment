55d2cd9ad6da2c1b0cfaf4e7505cad8d07b71b48
Donovan Mikrot <donovan.mikrot@perficient.com>
2015-11-13 11:07:01 -0600

    Hide peers, goals, engagements, and overall tabs from Peers

--- ent-talent-mgmt/src/main/webapp/scripts/app/entities/review/review-edit.controller.js
+++ ent-talent-mgmt/src/main/webapp/scripts/app/entities/review/review-edit.controller.js
@@ -1,13 +1,23 @@
 'use strict';
 
-angular.module('etmApp').controller('ReviewEditController', function ($scope, $stateParams, Review, ReviewType, ReviewStatus, User) {
+angular.module('etmApp').controller('ReviewEditController', function ($scope, $stateParams, Review, Principal) {
   $scope.review = {};
   $scope.load = function (id) {
-    Review.get({id: id}, function(result) {
+    Review.get({id: id}, function (result) {
       $scope.review = result;
     });
   };
-  
+
+  Principal.identity().then(function (account) {
+    $scope.allTabs = function () {
+      return $scope.review.id &&
+        (  $scope.review.reviewee.id === account.id
+        || $scope.review.reviewer.id === account.id
+        || $scope.review.reviewee.counselor.id === account.id
+        || $scope.review.reviewee.generalManager.id === account.id);
+    };
+  });
+
   if ($stateParams.review) {
     $scope.review = $stateParams.review;
   } else {
