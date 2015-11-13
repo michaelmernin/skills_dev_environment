4dfdb28b1389819c9a7f3375927d95c572f8634b
Alexandro Blanco <alex.blanco@perficient.com>
2015-11-13 16:57:01 -0600

    Adding more information to the review process. Creating base review_reminder process.

--- ent-talent-mgmt/src/test/java/com/perficient/etm/workflow/AnnualReviewTest.java
+++ ent-talent-mgmt/src/test/java/com/perficient/etm/workflow/AnnualReviewTest.java
@@ -1,6 +1,9 @@
 package com.perficient.etm.workflow;
 
+import static java.lang.System.out;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +18,6 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.perficient.etm.utils.SpringAppTest;
-import static java.lang.System.out;
 
 
 public class AnnualReviewTest extends SpringAppTest {
@@ -80,6 +82,125 @@ public class AnnualReviewTest extends SpringAppTest {
 	@Test(expected = ActivitiObjectNotFoundException.class)
 	public void testdeleteProcessNotFound(){
 		runtimeSvc.deleteProcessInstance("engagementReview:123", "Test Delete");
+	}
+	
+	@Test
+	public void testHappyPath(){
+		Map<String,Object> variables = getProcessVariables();
+		ProcessInstance processInstance = runtimeSvc.startProcessInstanceByKey("annualReview",variables);
+		
+		//Self review
+		Task t = completeAndGetNextTask(null,processInstance.getId());
+		assertNotNullAndAsignee(t,"Alex");
+		
+		//Reviewers review
+		t = completeAndGetNextTask(t,processInstance.getId());
+		assertNotNullAndAsignee(t,"Craig");
+		
+		//Directors review
+		t = completeAndGetNextTask(t,processInstance.getId());
+		assertNotNullAndAsignee(t,"Craig");
+		
+		//Joint review
+		t = completeAndGetNextTask(t,processInstance.getId());
+		assertNotNullAndAsignee(t,"Craig");
+		
+		//Final review
+		t = completeAndGetNextTask(t,processInstance.getId());
+		assertNotNullAndAsignee(t,"Craig");
+		
+		//Process should be done by now
+		t = completeAndGetNextTask(t,processInstance.getId());
+		assertNull(t);
+		
+		//TODO check this if historical data is enabled and check for isEnded() method. might be different
+		processInstance = runtimeSvc.createProcessInstanceQuery().processInstanceId(processInstance.getId()).singleResult();
+		assertNull(processInstance);
+	}
+	
+	@Test
+	public void testReviewersReject(){
+		Map<String,Object> variables = getProcessVariables();
+		ProcessInstance processInstance = runtimeSvc.startProcessInstanceByKey("annualReview",variables);
+		
+		//Self review
+		Task t = completeAndGetNextTask(null,processInstance.getId());
+		assertNotNullAndAsignee(t,"Alex");
+		
+		//Reviewers review
+		t = completeAndGetNextTask(t, processInstance.getId());
+		assertNotNullAndAsignee(t,"Craig");
+		//Fail the reviewers review
+		t = completeWithAndGetNextTask(t, "FAILURE", processInstance.getId());
+		
+		//flow has to go back to initial reviewer
+		assertNotNullAndAsignee(t,"Alex");
+		
+	}
+	
+	/**
+	 * Completes the current task with RESULT variable as SUCCESS
+	 * by calling the TaskService.complete method
+	 * @param t The Task object to complete
+	 */
+	private void completeATaskWithSucess(Task t){
+		completeATaskWith(t, "SUCCESS");
+	}
+	
+	/**
+	 * Completes the current task with RESULT variable as specified
+	 * in the parameters and by calling the TaskService.complete method
+	 * @param t The Task object to complete
+	 * @param result The String result to set in the complete variables
+	 */
+	private void completeATaskWith(Task t, String result){
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("RESULT", result);
+		taskSvc.complete(t.getId(), properties);
+	}
+	
+	/**
+	 * Completes the current Task t if the task is not null and retrieves the 
+	 * task where the process with processInstanceId landed. The task will
+	 * be completed with a RESULT = SUCCESS
+	 * @param t The Task to complete if necessary
+	 * @param processInstanceId The String process Instance id of the process
+	 * @return Task object if the process landed on a next task. Null if the process
+	 * ended
+	 */
+	private Task completeAndGetNextTask(Task t, String processInstanceId){
+		return completeWithAndGetNextTask(t,"SUCCESS", processInstanceId);
+	}
+	
+	/**
+	 * Completes the current Task t if the task is not null and retrieves the 
+	 * task where the process with processInstanceId landed. The task will
+	 * be completed with a RESULT = result param
+	 * @param t The Task to complete if necessary
+	 * @param result The String result to complete the task with
+	 * @param processInstanceId The String process Instance id of the process
+	 * @return Task object if the process landed on a next task. Null if the process
+	 * ended
+	 */
+	private Task completeWithAndGetNextTask(Task t, String result, String processInstanceId){
+		if (t != null)
+			completeATaskWith(t,result);
+		
+		//Reviewers Review
+		t = taskSvc.createTaskQuery().processInstanceId(processInstanceId).singleResult();
+		return t;
+	}
+	
+	/**
+	 * invokes assertNotNull in the passed task and assertEquals in Task.getAssignee() with the
+	 * assignee parameter
+	 * @param t The Task to assert
+	 * @param assignee The String value of assignee
+	 */
+	private void assertNotNullAndAsignee(Task t, String assignee){
+		assertNotNull(t);
+		assertEquals("Asignee for task "+t.getName() + " is not "+assignee+". Value = "+t.getAssignee()
+			,assignee, t.getAssignee());
 	}
 	
 }
