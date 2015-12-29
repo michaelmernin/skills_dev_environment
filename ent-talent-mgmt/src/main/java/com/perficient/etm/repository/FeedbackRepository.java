56eebddda5f899a28f5c61a55b184ac6fcc30098
Ahmed Musallam <ahmed.musallam@perficient.com>
2015-12-29 12:34:15 -0600

    Feedback security

--- ent-talent-mgmt/src/main/java/com/perficient/etm/repository/FeedbackRepository.java
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/repository/FeedbackRepository.java
@@ -1,7 +1,10 @@
 package com.perficient.etm.repository;
 
+import com.perficient.etm.authorize.FeedbackAuthorizer;
 import com.perficient.etm.domain.Feedback;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 
 import java.util.List;
 
@@ -9,10 +12,14 @@ import java.util.List;
  * Spring Data JPA repository for the Feedback entity.
  */
 public interface FeedbackRepository extends JpaRepository<Feedback,Long> {
-
+	@PostFilter(FeedbackAuthorizer.FILTER)
     @Query("select feedback from Feedback feedback where feedback.author.login = ?#{principal.username}")
     List<Feedback> findAllForCurrentUser();
 
+	@PostFilter(FeedbackAuthorizer.FILTER)
     List<Feedback> findAllByReviewId(Long reviewId);
+	
+	@PostAuthorize(FeedbackAuthorizer.AUTHORIZE)
+	Feedback findOne(Long id);
 
 }
