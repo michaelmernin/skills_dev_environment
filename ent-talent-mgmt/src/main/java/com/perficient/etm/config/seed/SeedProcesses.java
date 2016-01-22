ed90cbbaf48760a81b5b1bdbffe236e6cf7f758d
Donovan Mikrot <donovan.mikrot@perficient.com>
2016-01-21 19:06:37 -0600

    Create Activiti processes for all seeded reviews, cancel processes for closed reviews

--- ent-talent-mgmt/src/main/java/com/perficient/etm/config/seed/SeedProcesses.java
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/config/seed/SeedProcesses.java
@@ -1,26 +1,81 @@
 package com.perficient.etm.config.seed;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.inject.Inject;
 
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.perficient.etm.domain.Feedback;
+import com.perficient.etm.domain.Review;
 import com.perficient.etm.domain.ReviewStatus;
 import com.perficient.etm.repository.ReviewRepository;
+import com.perficient.etm.repository.UserRepository;
+import com.perficient.etm.security.SecurityUtils;
+import com.perficient.etm.security.UserDetailsService;
 import com.perficient.etm.service.ReviewService;
+import com.perficient.etm.service.activiti.ProcessService;
 
-public class SeedProcesses implements InitializingBean {
+public class SeedProcesses implements ApplicationListener<ContextRefreshedEvent> {
 
     @Inject
-    private ReviewService reviewService;
+    private UserRepository userRepository;
     
     @Inject
     private ReviewRepository reviewRepository;
     
+    @Inject
+    private ReviewService reviewService;
+    
+    @Inject 
+    private ProcessService processService;
+
     @Override
-    public void afterPropertiesSet() throws Exception {
-        reviewRepository.findAllByReviewStatus(ReviewStatus.OPEN).stream()
-            .forEach(review -> {
-                reviewService.startReviewProcess(review);
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        userRepository.findOneByLogin("system")
+            .map(UserDetailsService::mapUserDetails)
+            .ifPresent(systemUser -> {
+                SecurityUtils.runAs(systemUser, this::startAllActivitiProcesses);
             });
+    }
+
+    private void startAllActivitiProcesses() {
+        Stream<Review> reviewStream = reviewRepository.findAll().stream()
+            .filter(review -> review.getProcessId() == null)
+            .peek(reviewService::startReviewProcess);
+
+        completeTasksByStatus(reviewStream);
+    }
+
+    private void completeTasksByStatus(Stream<Review> reviewStream) {
+        Map<ReviewStatus, List<Review>> reviewStatusMap = reviewStream
+                .filter(review -> review.getReviewStatus() != ReviewStatus.OPEN)
+                .collect(Collectors.groupingBy(Review::getReviewStatus));
+
+        cancelProcesses(reviewStatusMap.get(ReviewStatus.CLOSED));
+        // TODO completeTasks(ReviewStatus.DIRECTOR_APPROVAL, reviewStatusMap.get(ReviewStatus.DIRECTOR_APPROVAL));
+        // TODO completeTasks(ReviewStatus.JOINT_APPROVAL, reviewStatusMap.get(ReviewStatus.JOINT_APPROVAL));
+        // TODO completeTasks(ReviewStatus.GM_APPROVAL, reviewStatusMap.get(ReviewStatus.GM_APPROVAL));
+        // TODO completeTasks(ReviewStatus.COMPLETE, reviewStatusMap.get(ReviewStatus.COMPLETE));
+    }
+
+    private void cancelProcesses(List<Review> reviews) {
+        reviews.stream()
+            .peek(review -> {
+                processService.cancel(review.getProcessId());
+            })
+            .map(Review::getFeedback)
+            .flatMap(Set::stream)
+            .map(Feedback::getProcessId)
+            .filter(Objects::nonNull)
+            .forEach(processService::cancel);
     }
 }
