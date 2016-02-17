534953475a28921bc02dc14c2469652ad939dbf6
Donovan Mikrot <donovan.mikrot@perficient.com>
2016-02-17 17:12:36 -0600

    Display actions available for peers dynamically

--- ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/peers/peers.controller.js
+++ ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/peers/peers.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('etmApp').controller('PeersController', function ($scope, $stateParams, $mdDialog, Review, User, Peer) {
+angular.module('etmApp').controller('PeersController', function ($scope, $stateParams, $mdDialog, Review, User, Peer, FeedbackStatus) {
   $scope.peers = [];
   var review = {};
   $scope.$parent.$watch('review', function (parentReview) {
@@ -9,11 +9,11 @@ angular.module('etmApp').controller('PeersController', function ($scope, $stateP
       $scope.peers = review.peers;
     }
   });
-  
+
   $scope.getMatches = function (query) {
      return User.autocomplete({query: query, reviewId: review.id}).$promise;
   };
-  
+
   $scope.peerSelected = function (user) {
     if (user != null) {
       if (user.login != null) {
@@ -22,37 +22,77 @@ angular.module('etmApp').controller('PeersController', function ($scope, $stateP
         window.location.reload();
       }
     }  
- };
- 
- $scope.deletePeer = function (user, ev) {
-   var confirmDelete = $mdDialog.confirm()
-     .title('Remove Peer from Review')
-     .ariaLabel('Remove Peer')
-     .content('Remove ' + user.firstName + ' ' + user.lastName + ' from this review?')
-     .ok('Remove')
-     .cancel('Cancel')
-     .targetEvent(ev);
-   $mdDialog.show(confirmDelete).then(function () {
-         $scope.peers.splice($scope.peers.indexOf(user), 1);
-         Peer.delete({reviewId: review.id, id: user.id});
-         window.location.reload();
-   });
- };
- 
- $scope.sendEmail = function () {
-   //TO DO: Add email functionality
-   var peerString = "Sending email(s) to ";
-   var peerSelected = false;
-   for (var peer in $scope.peers) {
-     if ($scope.peers[peer].selected) {
-       peerSelected = true;
-       peerString += $scope.peers[peer].firstName + " " + $scope.peers[peer].lastName + "... ";
-     }
-   }
-   if (peerSelected) {
-     alert(peerString);
-   } else {
-     alert("Select peers from the list below to send emails")
-   } 
-   };
+  };
+
+  $scope.deletePeer = function (user, ev) {
+    var confirmDelete = $mdDialog.confirm()
+      .title('Remove Peer from Review')
+      .ariaLabel('Remove Peer')
+      .content('Remove ' + user.firstName + ' ' + user.lastName + ' from this review?')
+      .ok('Remove')
+      .cancel('Cancel')
+      .targetEvent(ev);
+    $mdDialog.show(confirmDelete).then(angular.bind(null, deletePeer, user));
+  };
+
+  var allActions = [{
+    action: 'open',
+    validStatuses: [FeedbackStatus.NOT_SENT],
+    translateKey: 'review.peers.open',
+    fn: openPeers
+    
+  }, {
+    action: 'reopen',
+    validStatuses: [FeedbackStatus.READY],
+    translateKey: 'review.peers.reopen',
+    fn: reopenPeers
+  }, {
+    action: 'delete',
+    translateKey: 'review.peers.removePeer',
+    fn: deletePeers
+  }];
+  
+  $scope.availableActions = function () {
+    var selectedPeers = getSelectedPeers();
+    
+  };
+
+  $scope.performAction = function (action) {
+    action.fn(getSelectedPeers());
+  };
+
+  function getSelectedPeers() {
+    var selectedPeers = [];
+    angular.forEach($scope.peers, function (peer) {
+      if (peer.selected) {
+        selectedPeers.push(peer);
+        peer.selected = false;
+      }
+    });
+    return selectedPeers;
+  }
+  
+  function openPeers(peers) {
+    console.dir("Open Peers", peers);
+  }
+
+  function reopenPeers(peers) {
+    console.dir("Reopen Peers", peers);
+  }
+
+  function deletePeers(peers) {
+    var confirmDelete = $mdDialog.confirm()
+      .title('Remove Peers from Review')
+      .ariaLabel('Remove Peers')
+      .content('Remove ' + peers.length + 'peers from this review?')
+      .ok('Remove')
+      .cancel('Cancel')
+      .targetEvent(ev);
+    $mdDialog.show(confirmDelete).then(angular.bind(null, angular.each, peers, deletePeer));
+  }
+  
+  function deletePeer(peer) {
+    $scope.peers.splice($scope.peers.indexOf(peer), 1);
+    Peer.delete({reviewId: review.id, id: peer.id});
+  }
 });
