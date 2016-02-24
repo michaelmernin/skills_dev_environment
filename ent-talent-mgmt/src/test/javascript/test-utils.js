8a33071684de53f2768d77366b230a495e4eb8a4
Donovan Mikrot <donovan.mikrot@perficient.com>
2016-02-24 16:44:42 -0600

    Fix JS unit test to include new html request

--- ent-talent-mgmt/src/test/javascript/test-utils.js
+++ ent-talent-mgmt/src/test/javascript/test-utils.js
@@ -6,6 +6,7 @@ function stubInitRequests($httpBackend) {
   $httpBackend.whenGET('i18n/en/language.json').respond(200, '');
   $httpBackend.whenGET('scripts/components/toolbar/toolbar.html').respond({});
   $httpBackend.whenGET('scripts/components/sidenav/sidenav.html').respond({});
+  $httpBackend.whenGET('scripts/components/main/todo/todo-list.html').respond({});
   $httpBackend.whenGET('scripts/components/main/review/review-list.html').respond({});
   $httpBackend.whenGET('i18n/en/main.json').respond(200, '');
   $httpBackend.whenGET('scripts/app/main/main.html').respond({});
