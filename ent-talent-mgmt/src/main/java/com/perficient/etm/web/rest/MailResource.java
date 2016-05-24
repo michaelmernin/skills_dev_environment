af5f9759c5567c33a61c3ad4adb568c06ce9807a
Ahmed Musallam <ahmed.musallam@medtronic.com>
2016-05-24 17:50:18 -0500

    Replace Dumpster with Greenmail. Fix for email encoding and add privacy
    users will now only see only emails sent to them

--- ent-talent-mgmt/src/main/java/com/perficient/etm/web/rest/MailResource.java
+++ ent-talent-mgmt/src/main/java/com/perficient/etm/web/rest/MailResource.java
@@ -1,18 +1,15 @@
 package com.perficient.etm.web.rest;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.inject.Inject;
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeUtility;
+import javax.mail.internet.MimeMessage;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.geronimo.mail.util.QuotedPrintable;
-import org.apache.geronimo.mail.util.QuotedPrintableEncoder;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,10 +26,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codahale.metrics.annotation.Timed;
-import com.dumbster.smtp.MailMessage;
-import com.dumbster.smtp.SmtpServer;
+import com.icegreen.greenmail.util.GreenMail;
 import com.perficient.etm.repository.UserRepository;
 import com.perficient.etm.service.MailService;
+import com.perficient.etm.service.UserService;
 // TODO - remove this resource from production, only available in lower environments
 @RestController
 @RequestMapping("/api")
@@ -43,10 +40,10 @@ public class MailResource {
     private final String MESSAGE_HEADER_SUBJECT = "Subject";
 
     private final Logger log = LoggerFactory.getLogger(ReviewTypeResource.class);
-    private List<Integer> messageList = new ArrayList<Integer>();
+    List<Message> messages = new ArrayList<Message>();
 
     @Autowired(required=false)
-    protected SmtpServer smtpServer;
+    protected GreenMail smtpServer;
 
     @Inject
     JavaMailSenderImpl mailsender;
@@ -55,27 +52,28 @@ public class MailResource {
     MailService mailService;
     
     @Inject
-    private UserRepository userRepository;
+    private UserService userSvc;
 
     /**
      * GET /mail/messages -> get all the mail messages.
      */
     @RequestMapping(value = "/mail/messages", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Message> getAllMessages() {
+    public List<Message>  getAllMessages() {
         log.debug("REST request to get all mail messages");
-        MailMessage[] messages = smtpServer.getMessages();
-        List<Message> returnMessages = new ArrayList<Message>();
-        for (MailMessage message : messages) {
-            Message msg = new Message();
-            msg.setBody(decodeQP(message.getBody()));
-            msg.setFrom(message.getFirstHeaderValue(MESSAGE_HEADER_FROM));
-            msg.setTo(message.getFirstHeaderValue(MESSAGE_HEADER_TO));
-            msg.setSubject(message.getFirstHeaderValue(MESSAGE_HEADER_SUBJECT));
-            msg.setHashcode(message.hashCode());
-            returnMessages.add(msg);
-        }
-
-        return returnMessages;
+        List<Message> filteredMessages = new ArrayList<Message>();
+        Optional.ofNullable(smtpServer).map(GreenMail::getReceivedMessages).ifPresent(msgs -> {
+        	messages.clear();
+        	userSvc.getUserFromLogin().ifPresent(user -> {
+        		for (MimeMessage message : msgs) {
+            		Message m = new Message(message);
+                	messages.add(m);
+                	if(m.getTo().equals(user.getEmail())){
+                		filteredMessages.add(m);
+                	}
+                }
+            });
+        });
+        return filteredMessages;
     }
     /**
      * GET /mail/messages -> get all the mail messages.
@@ -84,11 +82,9 @@ public class MailResource {
     @ResponseBody
     public String getMessageByhashCode(@PathVariable int hash) {
         log.debug("REST request to get message with hashcode {}",hash);
-        MailMessage[] messages = smtpServer.getMessages();
-        for (MailMessage message : messages) {
-            int hashcode = message.hashCode();
-            if(message.hashCode() == hash){
-            	return decodeQP(message.getBody());
+		for (Message message : messages) {
+            if(message.getHashcode() == hash){
+            	return message.getBody();
             }
         }
        return "<p>NOT FOUND</p>";
@@ -109,7 +105,7 @@ public class MailResource {
         message.setText(
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
         mailsender.send(message);
-        messageList.add(message.hashCode());
+        messages.add(new Message(message));
         return new ResponseEntity<SimpleMailMessage>(HttpStatus.CREATED);
     }
 
@@ -120,13 +116,13 @@ public class MailResource {
     @Timed
     public ResponseEntity<SimpleMailMessage> clear() {
         log.debug("REST request to delete all mail");
-        smtpServer.clearMessages();
-        messageList.clear();
+        smtpServer.reset();
+        messages.clear();
         return new ResponseEntity<SimpleMailMessage>(HttpStatus.OK);
     }
 
     /**
-     * POST /mail/save -> send a new email.
+     * POST /mail/send -> send a new email.
      */
     @RequestMapping(value = "/mail/send", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
@@ -137,34 +133,9 @@ public class MailResource {
         message.setTo(msg.getTo());
         message.setSubject(msg.getSubject());
         message.setText(msg.getBody());
-        messageList.add(message.hashCode());
+        messages.add(new Message(message));
         mailsender.send(message);
         return new ResponseEntity<Message>(HttpStatus.OK);
-    }
-    
-    private String decodeQP(String qpString){
-    	String out="";
-    	InputStream inputStream = null;
-    	try {
-    		inputStream  = IOUtils.toInputStream(qpString);
-    		inputStream = MimeUtility.decode(inputStream, "quoted-printable");
-			out = IOUtils.toString(inputStream);
-		} catch (MessagingException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally{
-			Optional.ofNullable(inputStream).ifPresent(is -> {
-					try {
-						is.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			);
-		}
-    	
-        return out;
     }
     
 }
@@ -178,6 +149,36 @@ class Message{
     String id=null;
     int hashcode;
     public Message(){}
+    public Message(SimpleMailMessage message){
+    	this.to= StringUtils.join(message.getTo(),",");
+    	this.from = message.getFrom();
+    	this.subject = message.getSubject();
+    	this.body = message.getText();
+    	this.hashcode = message.hashCode();
+    }
+    public Message(MimeMessage mimeMessage){
+		Optional.ofNullable(mimeMessage).ifPresent(mime ->{
+			try {
+				Optional.ofNullable(mime.getAllRecipients()).ifPresent(recipients -> {
+					this.to = StringUtils.join(recipients, ",");
+				});
+				Optional.ofNullable(mime.getSender()).ifPresent(sender -> {
+					this.from = sender.toString();
+				});
+				Optional.ofNullable(mime.getSubject()).ifPresent(subject -> {
+					this.subject = subject;
+				});
+				Optional.ofNullable(mime.getContent()).ifPresent(content -> {
+					this.body = content.toString();
+				});
+				this.hashcode = mimeMessage.hashCode();
+			} catch (MessagingException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+					
+		});
+    }
     public String getTo() {
         return to;
     }
