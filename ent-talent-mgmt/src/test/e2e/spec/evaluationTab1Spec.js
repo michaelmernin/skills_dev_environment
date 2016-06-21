a8b5ab8517b6bf8fdd8f833eb9e74e271c371bc8
Ahmed Musallam <ahmed.musallam@medtronic.com>
2016-06-21 14:03:55 -0500

    a better way to handle async test for evaluation tab

--- ent-talent-mgmt/src/test/e2e/spec/evaluationTab1Spec.js
+++ ent-talent-mgmt/src/test/e2e/spec/evaluationTab1Spec.js
@@ -1,17 +1,17 @@
 /* DO NOT REMOVE: Protractor globals to be ignored by JsLint */
-/* globals require: false, describe: false, beforeAll: false, it: false, expect: false, protractor: fasle*/
+/* globals require: false, describe: false, beforeAll: false, it: false, expect: false, protractor: fasle, Promise: false*/
 
 var LoginPage = require('../page/loginPage.js');
 var userData = require('../data/userData.js');
 var EvaluationTabPage = require('../page/evaluationTabPage.js');
 
-describe('Enterprise Talent Management', function () {
+describe('Enterprise Talent Management', function() {
   'use strict';
-  describe('Evaluation Tab Page', function () {
+  describe('Evaluation Tab Page', function() {
     var evaluationTab;
     var EC;
 
-    beforeAll(function () {
+    beforeAll(function() {
       var loginPage = new LoginPage();
       loginPage.get();
       loginPage.login(userData.users.counselor);
@@ -19,31 +19,31 @@ describe('Enterprise Talent Management', function () {
       evaluationTab.get(1);
       EC = protractor.ExpectedConditions;
     });
-    
-    it('should be displayed when Evaluation tab is clicked', function () {
+
+    it('should be displayed when Evaluation tab is clicked', function() {
       expect(evaluationTab.ui.evaluationTabBtn.getText()).toBe('EVALUATION');
       expect(evaluationTab.ui.evaluationTabBtn.getAttribute('class')).toContain('md-ink-ripple md-active');
       expect(evaluationTab.ui.evaluationTabContent.getAttribute('class')).toContain('md-no-scroll md-active');
     });
-    
+
     //uncomment this once CDEV-456 is resolved
-   /*
-    it('- should allow to rate on Consulting Skills category', function(){
-        evaluationTab.getToggleQuestionnaire('Consulting Skills')
-          .then(function(questions){
-            questions.forEach(function(question){
-              // for loops need to be in a clousure to ensure proper execution
-              (function(q){
-                // wait for material animations to finish
-                browser.sleep(400);
-                q.click().then(function(){
-                  expect(evaluationTab.ui.evaluationForm.isPresent()).toBe(true);
-                  evaluationTab.ui.closeBtn.click();
-                });
-              })(question);
+
+    it('should allow to rate on Consulting Skills category', function() {
+      return evaluationTab.getToggleQuestionnaire('Consulting Skills')
+        .then(function(questions) {
+          // a dummy promise to start the chain
+          var chain = Promise.resolve();
+          questions.forEach(function(question) {
+            chain = chain.then(function() {
+              return question.click();
+            }).then(function() {
+              expect(evaluationTab.ui.evaluationForm.isPresent()).toBe(true);
+              return evaluationTab.ui.closeBtn.click();
             });
           });
-      });
-    */
+          return chain;
+        });
     });
+
+  });
 });
