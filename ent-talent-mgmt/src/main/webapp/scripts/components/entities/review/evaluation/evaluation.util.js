e12e51e471c0e48ed49c8f4ec59135f4c810d7bc
Ahmed Musallam <ahmed.musallam@kp.org>
2016-11-30 18:28:54 -0600

    remove undefined function

--- ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/evaluation/evaluation.util.js
+++ ent-talent-mgmt/src/main/webapp/scripts/components/entities/review/evaluation/evaluation.util.js
@@ -1,45 +1,45 @@
 'use strict';
 
 angular.module('etmApp').factory('EvaluationUtil', function (ReviewStatus) {
-  
+
   function eq(e1, e2) {
     if ((e1 === undefined || e1 === null) || (e2 === undefined || e2 === null)) {
       return e1 === e2;
     }
     return e1.id === e2.id;
   }
-  
+
   function isCounselor(review, user) {
     return eq(user, review.reviewee.counselor);
   }
-  
+
   function isGM(review, user) {
     return eq(user, review.reviewee.generalManager);
   }
-  
+
   function isDirector(review, user) {
-    return eq(user, review.reviewee.director) && !has([ReviewStatus.OPEN], review.reviewStatus);
+    return eq(user, review.reviewee.director);
   }
-  
+
   return {
     showAlways: function (review, user) {
       return review && review.reviewee && (isCounselor(review, user) || isGM(review, user) || isDirector(review, user));
     },
-    
+
     isCounselor: isCounselor,
-    
+
     isReviewee: function (review, user) {
       return eq(user, review.reviewee);
     },
-    
+
     isReviewer: function (review, user) {
       return eq(user, review.reviewer);
     },
-    
+
     isGM: isGM,
-    
+
     isDirector: isDirector,
-    
+
     isPeer: function (review, user) {
       var result = false;
       review.peers.forEach(function(peer) {
@@ -49,7 +49,7 @@ angular.module('etmApp').factory('EvaluationUtil', function (ReviewStatus) {
       });
       return result;
     },
-    
+
     eq: eq
   }
 });
