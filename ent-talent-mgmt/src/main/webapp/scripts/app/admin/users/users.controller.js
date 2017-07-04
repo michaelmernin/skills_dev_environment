901a35d259c67925dc6c58a3d8519103e0e22c04
Ahmed Musallam <ahmed.musallam@kp.org>
2017-07-03 20:18:29 -0500

    htm: use spaces. js, request user before opening edit dialog

--- ent-talent-mgmt/src/main/webapp/scripts/app/admin/users/users.controller.js
+++ ent-talent-mgmt/src/main/webapp/scripts/app/admin/users/users.controller.js
@@ -1,7 +1,17 @@
 'use strict';
 
-angular.module('etmApp').controller('UsersController', function ($scope, $mdDialog, User) {
+angular.module('etmApp').controller('UsersController', function ($scope, $mdDialog, $mdToast, User) {
   $scope.selectedUser = null;
+
+
+  function toast(str){
+    $mdToast.show(
+      $mdToast.simple()
+      .textContent(str)
+      .hideDelay(3000)
+    );
+  }
+
 
   $scope.selectUser = function(user) {
     $scope.selectedUser = user;
@@ -20,18 +30,31 @@ angular.module('etmApp').controller('UsersController', function ($scope, $mdDial
   };
 
   $scope.viewUserDetails = function (user, ev) {
-    $mdDialog.show({
-      controller: 'UserDetailController',
-      templateUrl: 'scripts/app/admin/users/user.detail.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      locals: {
-        user: user
-      }
-    }).then(function (updatedUser) {
-      angular.copy(updatedUser, user);
-      User.update(user);
-    });
+    if(user.id == null){
+      toast('userWithoutId error');
+      console.error("cannot edit user without id", user);
+      return;
+    }
+    User.get({id:user.login})
+    .$promise
+    .then(function(u){
+      $mdDialog.show({
+        controller: 'UserDetailController',
+        templateUrl: 'scripts/app/admin/users/user.detail.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        locals: {
+          user: u
+        }
+      }).then(function (updatedUser) {
+        angular.copy(updatedUser, user);
+        User.update(user)
+        .$promise
+        .then(function(user){
+          toast("updated user"+ user.login)
+        });
+      });
+    })
   };
 
   $scope.deleteUser = function (user, ev) {
