ebb5df67b66ac4347a27392e1e18e82a8eb91e7d
Brian Madison <brian.madison@perficient.com>
2017-07-18 10:23:03 -0500

    CDEV-544 Prevent access to Dashboard as an un-authenticated user.

--- ent-talent-mgmt/src/main/webapp/scripts/app/main/main.controller.js
+++ ent-talent-mgmt/src/main/webapp/scripts/app/main/main.controller.js
@@ -1,10 +1,14 @@
 'use strict';
 
-angular.module('etmApp').controller('MainController', function ($scope, Principal) {
+angular.module('etmApp').controller('MainController', function ($scope, Principal, $state) {
   $scope.account = {};
   $scope.isAuthenticated = Principal.isAuthenticated;
 
   Principal.identity().then(function (account) {
     $scope.account = account || {};
+
+    if (!$scope.isAuthenticated()) {
+      $state.go('login');
+    }
   });
 });
